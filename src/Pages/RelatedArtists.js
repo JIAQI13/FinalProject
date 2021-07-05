d1836181ebc53e73a88386b62c0a3b6340eb7d60
davidclaveau <daveclaveau@gmail.com>
2021-07-04 19:13:23 -0700

    Add related artists query and component - WIP

--- src/Pages/RelatedArtists.js
+++ src/Pages/RelatedArtists.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import gql from 'graphql-tag';
 import { useParams } from 'react-router';
-// import { Query } from 'react-apollo';
-// import BubblesGraph from '../components/BubblesGraph';
+import { Query } from 'react-apollo';
 
 export const GET_TRACK = gql`
   query GetTrack {
@@ -24,15 +23,13 @@ export default function RelatedArtists () {
   const { id } = useParams();
 
   return (
-    <>
-      <h1>Hello</h1>
-    </>
-    // <div>
-    //   <Query query={GET_TRACK}>
-    //     {({ loading, data }) => !loading && (
-    //       <BubblesGraph graphData={data} dataSet={"topTracksPopularity"}></BubblesGraph>
-    //     )}
-    //   </Query>
-    // </div>
+    <h1>Hello</h1>
+  //   <div>
+  //     <Query query={GET_TRACK}>
+  //       {({ loading, data }) => !loading && (
+  //         <p>{"hello"}</p>
+  //       )}
+  //     </Query>
+  //   </div>
   );
 }
