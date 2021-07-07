8aabd7bc7b47f3aee0be4cb5ad82516e767002ab
davidclaveau <daveclaveau@gmail.com>
2021-07-07 14:13:06 -0700

    Fix small error

--- src/components/HeatMap.js
+++ src/components/HeatMap.js
@@ -1,13 +1,20 @@
 import React, { useRef, useEffect } from "react";
 import * as d3 from 'd3';
+import HeatMapDate from 'react-d3-heatmap'
 import useWindowDimensions from '../helpers/userWindowDimensions'
 
 export default function HeatMap(props) {
   const { height, width } = useWindowDimensions();
 
-  console.log("data", props.graphData)
+  console.log("data", props.graphData);
 
   return (
-    <h1>Howdy</h1>
+    <HeatMapDate
+      startDate={startDate}
+      endDate={endDate}
+      data={data}
+      colors={colors}
+      displayLegend
+    />
   );
 }
