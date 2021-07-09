e7284183dd7f0eecd296f0a63817fa13ad086128
davidclaveau <daveclaveau@gmail.com>
2021-07-09 08:57:58 -0700

    Fix chord chart and add some styling

--- src/Pages/TopArtistsGenres.js
+++ src/Pages/TopArtistsGenres.js
@@ -2,6 +2,7 @@ import React from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import ChordChart from '../components/ChordChart';
+import Loader from "react-loader-spinner";
 
 export const GET_ARTIST = gql`
   query GetArtist {
@@ -19,12 +20,33 @@ export const GET_ARTIST = gql`
 `;
 
 export default function TopArtistsPopularity () {
+  const tmpStyle = {
+    "display": 'flex',
+    "justify-content": "center",
+    "align-content": "center",
+    "padding-top": "30%"
+  };
+
   return (
     <div>
       <Query query={GET_ARTIST}>
-        {({ loading, data }) => !loading && (
-          <ChordChart graphData={data}></ChordChart>
-        )}
+        {({ loading, data }) => {
+          if (loading) {
+            return (
+              <div style={tmpStyle}>
+                <Loader
+                  type="Bars"
+                  color="#57F289"
+                  height={100}
+                  width={100}
+                />
+              </div>
+            );
+          }
+          return(
+            <ChordChart graphData={data}></ChordChart>
+          );
+        }}
       </Query>
     </div>
   );
